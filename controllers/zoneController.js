8f657cb945c737765fb30257ed4e3a190cd8e518
Eva Vides <esvides@unicesar.edu.co>
2023-11-29 15:16:51 -0500

    cambios con recurso a

--- controllers/zoneController.js
+++ controllers/zoneController.js
@@ -2,26 +2,51 @@ const { json } = require("express/lib/response");
 const { Op } = require("sequelize");
 const Zone = require("../models/Zone");
 const { use } = require("../routes/api");
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const fs = require('fs');
+const mime = require('mime-types');
+
+
+
+// Crea una nueva instancia del servicio S3
+const s3 = new AWS.S3();
+const upload = multer();
 
 exports.createZone = async (req, res) => {
     try {
-        const { nombre, capacidad,idCategoria,foto, descripcion,noPermitido,disponibilidad} = req.body;
+
+const base64String="";
+        const { nombre, capacidad, idCategoria, foto, descripcion, noPermitido, disponibilidad } = req.body;
+        const imageBuffer = Buffer.from(foto, 'base64');
+        
+        // Configurar la instancia S3
+        const s3 = new AWS.S3();
+        console.log('Valor de foto:', foto);
+        const s3Params = {
+            Bucket: bucketName,
+            Key: nombre,
+            Body: imageBuffer,
+            ContentType: 'image/png', 
+        };
+        // Subir la imagen a S3
+        const s3UploadResult = await s3.upload(s3Params).promise();
+        const fotoURL = s3UploadResult.Location; // La URL de la imagen en S3
         const z = {
             'idCuenta': req.user.idCuenta,
             'nombre': nombre,
             'capacidad': capacidad,
             'idCategoria': idCategoria,
-            'foto': foto,
+            'foto': fotoURL,
             'estado': 1,
             'descripcion': descripcion,
             'noPermitido': noPermitido,
             'disponibilidad': disponibilidad,
-           
-           
         };
+        
         const zone = await Zone.create(z);
         res.status(200).send(zone);
-
+        console.log("Esta es la foto de la url: ", fotoURL)
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
@@ -34,8 +59,8 @@ exports.listZone = async (req, res) => {
                 estado: {
                     [Op.or]: [1, 2]
                 },
-                idCuenta:req.user.idCuenta
-              }
+                idCuenta: req.user.idCuenta
+            }
         });
         res.status(200).send(zones);
     } catch (error) {
@@ -45,13 +70,13 @@ exports.listZone = async (req, res) => {
 }
 exports.getZone = async (req, res) => {
     try {
-        
+
         const zoneId = req.params.id;
         const zone = await Zone.findByPk(zoneId);
         if (!zone) {
             res.status(404).json({ message: 'Zona no encontrada' });
         }
-      
+
         res.status(200).send(zone);
     } catch (error) {
         console.log(error);
@@ -59,14 +84,14 @@ exports.getZone = async (req, res) => {
     }
 }
 exports.updateZone = async (req, res) => {
-    try {  
-        const {id, nombre, capacidad,idCategoria,foto, descripcion,noPermitido,disponibilidad, estado} = req.body;
+    try {
+        const { id, nombre, capacidad, idCategoria, foto, descripcion, noPermitido, disponibilidad, estado } = req.body;
         const zoneId = id;
         const zone = await Zone.findByPk(zoneId);
         if (!zone) {
             res.status(404).json({ message: 'Zona no encontrado' });
         }
-      
+
         zone.nombre = nombre;
         zone.capacidad = capacidad;
         zone.idCategoria = idCategoria;
@@ -85,7 +110,6 @@ exports.updateZone = async (req, res) => {
 }
 exports.deleteZone = async (req, res) => {
     try {
-
         const zoneId = req.params.id;
         const zone = await Zone.findByPk(zoneId);
         if (!zone) {
